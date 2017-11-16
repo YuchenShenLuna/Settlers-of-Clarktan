6768f73e06b90a818910fdef03e6de327df98ba7
Yuchen Shen <luna@dhcp-ccc-2688.redrover.cornell.edu>
2017-11-16 11:54:43 -0500

    changed checkzip

--- checkzip.sh
+++ checkzip.sh
@@ -3,17 +3,10 @@
 
 zip=good
 
-if [ -e a2src.zip ]; then
-  echo "a2src.zip exists.  Good."
-  if zipinfo -1 a2src.zip | grep -q "^state.ml$"; then
-    echo "state.ml is in the root folder of the zip.  Good."
-  else 
-    echo "state.ml is NOT in the root folder of the zip.  This is bad."
-    echo "Did you create the zip by running \"make zip\"?"
-  zip=bad
-fi
+if [ -e finalsrc.zip ]; then
+  echo "finalsrc.zip exists.  Good."
 else
-  echo "a2src.zip does NOT exist.  This is bad.  Have you run \"make zip\"?"
+  echo "finalsrc.zip does NOT exist.  This is bad.  Have you run \"make zip\"?"
   zip=bad
 fi
 
@@ -26,12 +19,12 @@ Your zip file looks good to me.  Congratulations!
 I checked to make sure your zip contains at least one of
 your source files, but I can't check to make sure it contains
 all of them, because I can't know what modules or test files
-you might have added.  As long as you created the zip by running 
-"make zip", it will have them.  But just in case you want to 
+you might have added.  As long as you created the zip by running
+"make zip", it will have them.  But just in case you want to
 double check, here are all the files I found in your zip:
 
 EOF
-  zipinfo -1 a2src.zip
+  zipinfo -1 finalsrc.zip
 else
   cat <<EOF
 ===========================================================
@@ -40,10 +33,10 @@ WARNING
 Your zip file looks broken to me.  The code that
 you submit might not compile on the grader's machine,
 leading to heavy penalties.  Please fix your zip file.
-Check the error messages above carefully to determine 
+Check the error messages above carefully to determine
 what is wrong with your file.  The most common mistake
 is creating the zip using your OS's file browser, instead
-of using "make zip".  See a consultant for help if you 
+of using "make zip".  See a consultant for help if you
 cannot determine what is wrong.
 ===========================================================
 EOF
