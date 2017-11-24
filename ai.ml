2e065d9fcdc6de85b790cdf116d80203531d300b
Yuchen Shen <luna@dhcp-rhodes-3021.redrover.cornell.edu>
2017-11-24 12:28:02 -0500

    added AI modules

---
+++ ai.ml
@@ -0,0 +1,13 @@
+open Tile
+
+open DevCard
+
+open Building
+
+open Player
+
+type ai
+
+let init_ai = failwith "TODO"
+
+let init_ai_list = failwith "TODO"
---
+++ ai.mli
@@ -0,0 +1,13 @@
+open Tile
+
+open DevCard
+
+open Building
+
+open Player
+
+type ai
+
+val init_ai: color -> ai
+
+val init_ai_list : ai list
