eb6b1ebad39cd597af31c9c23558992f539dc344
dustspeck <ejj35@cornell.edu>
2017-11-24 21:48:50 -0500

    Added numbered tokens to GUI

--- gui.ml
+++ gui.ml
@@ -3,8 +3,14 @@ let rec round = function
   | h :: t -> (h |> fst |> int_of_float, h |> snd |> int_of_float) :: round t
 
 let draw_canvas () =
-  Graphics.clear_graph ();
-  let f t = t |> Tile.corners |> round |> Array.of_list |> Graphics.draw_poly in
+  let open Graphics in
+  clear_graph ();
+  let f t = t |> Tile.corners |> round |> Array.of_list |> draw_poly;
+    let x = t.center |> fst |> (-.) (0.1 *. t.edge) |> (~-.) |> int_of_float in
+    let y = t.center |> snd |> (-.) (0.1 *. t.edge) |> (~-.) |> int_of_float in
+    moveto x y;
+    t.dice |> string_of_int |> draw_string
+  in
   List.iter f (State.init_canvas ()).tiles
 
 let draw_robber = failwith "TODO"
