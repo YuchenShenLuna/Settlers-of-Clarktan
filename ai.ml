82f824ed6ecdda2b9fafe2d1c699af62f1c0e977
Yuchen Shen <luna@dhcp-hol-3025.redrover.cornell.edu>
2017-11-27 15:15:12 -0500

    done skeleton code for AI

--- ai.ml
+++ ai.ml
@@ -1,1 +1,39 @@
 open Elements
+
+let init_choose_road_build = failwith "TODO"
+
+let init_choose_settlement_build = failwith "TODO"
+
+let choose_road = failwith "TODO"
+
+let choose_settlement = failwith "TODO"
+
+let choose_city = failwith "TODO"
+
+let want_build_settlement = failwith "TODO"
+
+let want_build_road = failwith "TODO"
+
+let want_build_city = failwith "TODO"
+
+let want_init_trade = failwith "TODO"
+
+let init_trade = failwith "TODO"
+
+let want_accept_trade_player = failwith "TODO"
+
+let want_trade_bank = failwith "TODO"
+
+let want_trade_ports = failwith "TODO"
+
+let want_buy_card = failwith "TODO"
+
+let want_play_monopoly = failwith "TODO"
+
+let want_play_year_of_plenty = failwith "TODO"
+
+let want_play_road_building = failwith "TODO"
+
+let want_play_knight = failwith "TODO"
+
+let choose_robber_spot = failwith "TODO"
