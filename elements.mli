86e8a4218a1071a235832fda793528f843b93b58
Yuchen Shen <luna@dhcp-hol-3025.redrover.cornell.edu>
2017-11-27 14:54:47 -0500

    updated comments for Element

--- elements.mli
+++ elements.mli
@@ -1,3 +1,5 @@
+(* Elements module keeps useful informations used throughout the project *)
+
 (* color is the type of different colors for different players *)
 type color = Red | Yellow | Blue | Green | White
 
